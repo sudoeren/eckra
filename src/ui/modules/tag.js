47f6e2e88187074b93daf444163c600867f07a56
[methlangg] <123836990+sudoeren@users.noreply.github.com>
2026-03-11 14:45:01 +0300

    feat: update version to 1.0.8 in README.md and index.js; adjust tag prompt example

--- src/ui/modules/tag.js
+++ src/ui/modules/tag.js
@@ -44,7 +44,7 @@ async function doTag() {
       {
         type: "input",
         name: "name",
-        message: s.muted("Tag name (e.g. v1.0.7):"),
+        message: s.muted("Tag name (e.g. v1.0.8):"),
         validate: (v) => v.length > 0,
       },
     ]);
